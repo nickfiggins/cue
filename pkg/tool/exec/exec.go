c5b25069db539de8587ac2cfa6738bd44674d08a
Nick Figgins <figginsn@gmail.com>
2023-12-27 18:27:59 -0500

    pkg/tool/exec: remove deprecated method calls, handle TODO
    This change replaces calls to the deprecated Lookup() function
    with calls to LookupPath() in exec.Run. It also handles a TODO
    to internally use exec.Run's cue template to define a default
    for mustSucceed, rather than setting it inside the function.
    
    Signed-off-by: Nick Figgins <figginsn@gmail.com>

--- pkg/tool/exec/exec.go
+++ pkg/tool/exec/exec.go
@@ -31,10 +31,14 @@ func init() {
 	task.Register("exec", newExecCmd)
 }
 
-type execCmd struct{}
+type execCmd struct {
+	schema cue.Value
+}
 
 func newExecCmd(v cue.Value) (task.Runner, error) {
-	return &execCmd{}, nil
+	return &execCmd{
+		schema: v,
+	}, nil
 }
 
 func (c *execCmd) Run(ctx *task.Context) (res interface{}, err error) {
@@ -43,15 +47,12 @@ func (c *execCmd) Run(ctx *task.Context) (res interface{}, err error) {
 		return cue.Value{}, err
 	}
 
+	object := ctx.Obj.Unify(c.schema)
+
 	// TODO: set environment variables, if defined.
 	stream := func(name string) (stream cue.Value, ok bool) {
-		c := ctx.Obj.Lookup(name)
-		// Although the schema defines a default versions, older implementations
-		// may not use it yet.
-		if !c.Exists() {
-			return
-		}
-		if err := c.Null(); ctx.Err != nil || err == nil {
+		c := object.LookupPath(cue.ParsePath(name))
+		if err := c.Null(); c.Err() != nil || err == nil {
 			return
 		}
 		return c, true
@@ -71,14 +72,10 @@ func (c *execCmd) Run(ctx *task.Context) (res interface{}, err error) {
 		cmd.Stderr = ctx.Stderr
 	}
 
-	// TODO(mvdan): exec.Run declares mustSucceed as a regular field with a default of true.
-	// We should be able to rely on that here, removing the need for Exists and repeating the default.
-	mustSucceed := true
-	if v := ctx.Obj.LookupPath(cue.ParsePath("mustSucceed")); v.Exists() {
-		mustSucceed, err = v.Bool()
-		if err != nil {
-			return nil, errors.Wrapf(err, v.Pos(), "invalid bool value")
-		}
+	v := object.LookupPath(cue.ParsePath("mustSucceed"))
+	mustSucceed, err := v.Bool()
+	if err != nil {
+		return nil, errors.Wrapf(err, v.Pos(), "invalid bool value")
 	}
 
 	update := map[string]interface{}{}
@@ -102,11 +99,13 @@ func (c *execCmd) Run(ctx *task.Context) (res interface{}, err error) {
 			update["stderr"] = err.Error()
 		}
 	}
-	if !mustSucceed {
-		return update, nil
+
+	if mustSucceed {
+		return nil, fmt.Errorf("command %q failed: %v", doc, err)
 	}
 
-	return nil, fmt.Errorf("command %q failed: %v", doc, err)
+	return update, nil
+
 }
 
 func mkCommand(ctx *task.Context) (c *exec.Cmd, doc string, err error) {
@@ -152,10 +151,9 @@ func mkCommand(ctx *task.Context) (c *exec.Cmd, doc string, err error) {
 
 	cmd := exec.CommandContext(ctx.Context, bin, args...)
 
-	cmd.Dir, _ = ctx.Obj.Lookup("dir").String()
+	cmd.Dir, _ = ctx.Obj.LookupPath(cue.ParsePath("dir")).String()
 
-	env := ctx.Obj.Lookup("env")
-
+	env := ctx.Obj.LookupPath(cue.ParsePath("env"))
 	// List case.
 	for iter, _ := env.List(); iter.Next(); {
 		str, err := iter.Value().String()
@@ -167,7 +165,7 @@ func mkCommand(ctx *task.Context) (c *exec.Cmd, doc string, err error) {
 	}
 
 	// Struct case.
-	for iter, _ := ctx.Obj.Lookup("env").Fields(); iter.Next(); {
+	for iter, _ := env.Fields(); iter.Next(); {
 		label := iter.Label()
 		v := iter.Value()
 		var str string
